93749a0fe2f03c525ead875fd6a1fec83553638d
nshuti jonathan <nshutijonathan130@gmail.com>
2019-09-23 15:30:04 +0200

    master(Readme.md):added README.md

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # codepirates-ah-frontend
 
 [![Build Status](https://travis-ci.org/andela/codepirates-ah-frontend.svg?branch=develop)](https://travis-ci.org/andela/codepirates-ah-frontend)
+[![Maintainability](https://api.codeclimate.com/v1/badges/507ef8feadf79bfa96c7/maintainability)](https://codeclimate.com/github/andela/codepirates-ah-frontend/maintainability)
+
+[![Test Coverage](https://api.codeclimate.com/v1/badges/507ef8feadf79bfa96c7/test_coverage)](https://codeclimate.com/github/andela/codepirates-ah-frontend/test_coverage)
--- src/components/testpage.js
+++ src/components/testpage.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import logo from '../images/logo.png';
-
+import React from "react";
+import logo from "../images/logo.png";
+import style from "./testpage.scss";
 const TestPage = () => (
-  <div style={{ textAlign: 'center' }}>
+  <div style={{ textAlign: "center" }}>
     <img src={logo} alt="logo" />
-    <h3 style={{ color: 'navy' }}>Welcome to Authors Haven</h3>
+    <h3 style={{ color: "navy" }}>Welcome to Authors Haven</h3>
+    <div className={style}>
+      <p>Sass loaders are working now!</p>
+    </div>
   </div>
 );
 
