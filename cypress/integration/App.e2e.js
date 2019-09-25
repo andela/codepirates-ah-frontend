30915601a9d2d12dcd9e7bd575d031e4cd30e2f3
nkalyesubula <noahkalyesubula@gmail.com>
2019-09-25 09:35:12 +0200

    rebase

---
+++ cypress/integration/App.e2e.js
@@ -0,0 +1,12 @@
+describe('App E2E', () => {
+    it('should assert that true is equal to true', () => {
+      expect(true).to.equal(true);
+    });
+  });
+// describe('App E2E', () => {
+//     it('should have a header', () => {
+//       cy.visit('http://localhost:8080');
+//       cy.get('h1')
+//         .should('have.text', 'My Counter');
+//     });
+//   });
