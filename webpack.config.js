7504b327d02a96472c2a6e3c48260bf86539042a
Anguandia <mikeanguandia@gmail.com>
2019-09-25 06:05:36 +0200

    remove babel config from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -32,17 +32,7 @@ module.exports = (env) => ({
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', {
-                debug: true,
-                useBuiltIns: 'usage',
-                corejs: 3,
-              }],
-              '@babel/preset-react',
-            ],
-            plugins: [['@babel/plugin-proposal-class-properties', { loose: true }]],
-          },
+          options: {},
         },
       },
       {
