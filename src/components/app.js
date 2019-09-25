efa687f3ee0f7e12e30fbdd21e874f314341461e
salviosage <salviosage@gmail.com>
2019-09-25 21:39:58 +0200

    routing (finish setup routing) [Finishes #167313356]

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD:src/components/testpage.js
 import React from "react";
 import logo from "../images/logo.png";
 import style from "./testpage.scss";
 const TestPage = () => (
   <div style={{ textAlign: "center" }}>
-=======
-import React from 'react';
-import logo from '../images/logo.png';
-
-const App = () => (
-  <div style={{ textAlign: 'center' }}>
->>>>>>> routing (finish setup routing):src/components/app.js
     <img src={logo} alt="logo" />
     <h3 style={{ color: "navy" }}>Welcome to Authors Haven</h3>
     <div className={style}>
