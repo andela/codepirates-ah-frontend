9729eab1cce13026b54b038a4fa86e4af0f37e3d
nkalyesubula <noahkalyesubula@gmail.com>
2019-09-26 11:35:23 +0200

    chore(end-to-end): Write end-to-end tests for the application [Finishes #167313354]

--- cypress/integration/App.e2e.js
+++ cypress/integration/App.e2e.js
@@ -1,12 +1,7 @@
 describe('App E2E', () => {
-    it('should assert that true is equal to true', () => {
-      expect(true).to.equal(true);
-    });
+  it('should have a header', () => {
+    cy.visit('/');
+    cy.get('h3')
+      .should('have.text', 'Welcome to Authors Haven');
   });
-describe('App E2E', () => {
-    it('should have a header', () => {
-      cy.visit('/');
-      cy.get('h3')
-        .should('have.text', 'Welcome to Authors Haven');
-    });
-  });
+});
